93f7c5c31b197573f054bda4a7a8059a75ad30bb
fudge <auroraborus@proton.me>
2026-07-31 20:48:34 -0700

    updated num.py, readme

--- Math_Tools/num.py
+++ Math_Tools/num.py
@@ -1,32 +1,67 @@
 import random as rand
 
-count = 0
-blue = 0
-pink = 0
-gray = 0
-orange = 0
-n = int(input("input desired number of desired trials\n"))
 
-while count < n:
+def rng():
 
-    num = rand.randint(1, 100)
+    count = 0
+    blue = 0
+    pink = 0
+    gray = 0
+    orange = 0
+    n = int(input("input desired number of desired trials\n"))
 
-    if num < 41:
-        orange+=1
+    while count < n:
 
-    if 40 < num < 66:
-        blue +=1
+        num = rand.randint(1, 100)
 
-    if 65 < num < 91:
-        pink +=1
+        if num < 41:
+            orange+=1
 
-    if 90 < num:
-        gray +=1
+        if 40 < num < 66:
+            blue +=1
 
-    count+=1
+        if 65 < num < 91:
+            pink +=1
 
-print("Blue: " + str(blue) + " (" + str(100*(blue/n)) + "%)\n"
-      "Pink: " + str(pink) + " (" + str(100*(pink/n)) + "%)\n"
-      "Gray: " + str(gray) + " (" + str(100*(gray/n)) + "%)\n"
-      "Orange: " + str(orange) + " (" + str(100*(orange/n)) + "%)\n")
+        if 90 < num:
+            gray +=1
+
+        count+=1
+
+    print("Blue: " + str(blue) + " (" + str(100*(blue/n)) + "%)\n"
+        "Pink: " + str(pink) + " (" + str(100*(pink/n)) + "%)\n"
+        "Gray: " + str(gray) + " (" + str(100*(gray/n)) + "%)\n"
+        "Orange: " + str(orange) + " (" + str(100*(orange/n)) + "%)\n")
+
+def coinToss():
+
+    n = int(input("input desired number of desired coint tosses\n"))
+    count = 0
+    heads = 0
+    tails = 0
+
+    while count < n:
+
+        num = rand.randint(0, 1)
+
+        if num == 0:
+            heads+=1
+        else:
+            tails +=1
+
+        count +=1
+
+    print("Expected heads: " + str(int(n/2)) + " (50%)" +
+          "\n Actual heads: " + str(heads) + " (" + str(100*heads/n) + "%)" +
+          "\n Expected tails: " + str(int(n/2)) + " (50%)" +
+          "\n Actual tails: " + str(tails) + " (" + str(100*tails/n) + "%)")
+
+q = input("Activate random number generator or coin flipper? (answer rng or cointoss)\n")
+
+if q == "rng":
+    rng()
+elif q == "cointoss":
+    coinToss()
+else:
+    print("input not understood")
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-There's nothing here worth looking at
+practicing python by making math tools while also working on html and js. there's not much here yet; come back later!
